95a3f813967de6159229e3e03276001de2c5dca8
Lclocker <47572247+Lclocker@users.noreply.github.com>
2022-04-28 21:44:16 -0300

    PressurePlate wip

---
+++ Assets/Scripts/EnergyModule/EnergyObjects/PressurePlate.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnergySource))]
+public class PressurePlate : MonoBehaviour
+{
+  private BoxCollider2D colider;
+  private EnergySource energy;
+  // Start is called before the first frame update
+  void Start()
+  {
+    this.colider = gameObject.GetComponent<BoxCollider2D>();
+    this.energy = gameObject.GetComponent<EnergySource>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void OnTriggerEnter2D(Collider2D otherCollider)
+  {
+    if (otherCollider.gameObject.tag == "Player")
+    {
+      gameObject.transform.Translate(0f, -0.025f, 0f);
+      this.energy.ToggleState();
+    }
+
+  }
+  private void OnTriggerExit2D(Collider2D otherCollider)
+  {
+    if (otherCollider.gameObject.tag == "Player")
+    {
+      gameObject.transform.Translate(0f, +0.025f, 0f);
+      this.energy.ToggleState();
+    }
+
+  }
+}
